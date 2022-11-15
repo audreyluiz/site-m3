8631b98b985451e23ea3c5229757b98fb40b165c
Audrey Luiz <audreyluiz.ls@gmail.com>
2022-11-15 13:05:58 -0300

    iniciando projeto

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>M3 Telecom</h1>
+        <h2>Conectando Você ao Mundo!</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=ola">
+                            Planos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=html">
+                            Integração HTML
+                            </a>
+                        </li><li>
+                            <a href="exercicio.php?dir=basico&file=css">
+                            Integração PHP
+                            </a>
+                        </li>
+                    </ul>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+    Deus é o segredo do sucesso.
+    </footer>
+</body>
+</html>
